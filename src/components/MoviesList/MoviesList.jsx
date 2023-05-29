de32050d6993ae88b2638947a4a3f0c426ccfbef
DTripleD <tripledpadik@gmail.com>
2023-05-29 22:43:49 +0300

    make styles

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,16 +1,23 @@
 import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const MoviesList = ({ films }) => {
   const location = useLocation();
+    const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
+
+  console.log(films)
 
   return (
     <div>
       <ul>
         {films?.map(film => (
           <li key={film.id}>
+            <div>
+              <img src={IMAGES_BASE_URL + film.poster_path } alt="" width='200px'/>
             <Link to={`/movies/${film.id}`} state={{ from: location }}>
               {film.name || film.title}
             </Link>
+            </div>
           </li>
         ))}
       </ul>
@@ -19,3 +26,7 @@ const MoviesList = ({ films }) => {
 };
 
 export default MoviesList;
+
+// MoviesList.propTypes = {
+//   films: PropTypes.arrayOf{ },
+// };
